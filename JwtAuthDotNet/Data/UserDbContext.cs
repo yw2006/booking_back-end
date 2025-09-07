264b965285136a9db014a446d8e0efaf53641000
Joe-arch <youssef240100538@sut.edu.eg>
2025-09-07 11:35:31 +0300

    Added Tables to DbContext and added created at and updated at to User.cs

--- JwtAuthDotNet/Data/UserDbContext.cs
+++ JwtAuthDotNet/Data/UserDbContext.cs
@@ -6,5 +6,9 @@ namespace JwtAuthDotNet.Data
     public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
     }
 }
